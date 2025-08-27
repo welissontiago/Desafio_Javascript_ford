6725e29fd4a074211fed469593e76e030bfcc6f7
UELISSON TIAGO SILVA SANTOS <020.732974@universidadesenaicimatec.edu.br>
2025-08-27 14:35:32 -0300

    Finalização do projeto

--- Projeto/js/form.js
+++ Projeto/js/form.js
@@ -1,27 +1,29 @@
-
 //class contato
 
 class contato {
-    
+  constructor(nome, email, telefone, tipo, mensagem) {
+    this.nome = nome;
+    this.email = email;
+    this.telefone = telefone;
+    this.tipo = tipo;
+    this.mensagem = mensagem;
+  }
 }
 
 function Post(form) {
+  let data = new contato(
+    form.elements.namedItem("nome").value,
+    form.elements.namedItem("email").value,
+    form.elements.namedItem("telefone").value,
+    form.elements.namedItem("tipo").value,
+    form.elements.namedItem("mensagem").value
+  );
 
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
-            form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
-            form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
-  
-}
+  console.log("Dados do formulário ", data);
 
-function Enviar() {
+  alert(
+    `Obrigado ${data.nome}! Sua menssagem de ${data.tipo} foi enviada com sucesso`
+  );
 
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-    }
-
+  form.reset();
 }
